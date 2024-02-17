d05f8dad18cd56393bfd68e61255caa6a2980f30
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2024-02-17 09:25:54 +0100

    fix: to match the updated version of sonic_engine

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,8 +1,17 @@
+import os
+import sys
+
+# TODO: if 'dev', insert the parent of the parent directory to include the sonic-engine package
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+sys.path.insert(0, parent_parent_dir)
+
 from sonic_engine.core.engine import Engine
 
 
 def main():
-    engine = Engine((__file__, "./config.yaml"))
+    file_name = "../config.yaml"
+    engine = Engine(os.path.join(__file__, file_name))
     engine.start()
 
 
